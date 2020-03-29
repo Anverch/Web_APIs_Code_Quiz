79c929386023716fb71672679b2c181c8c71ba09
Aleksandr Saiyan <anverch@gmail.com>
2020-03-29 18:44:27 -0500

    add timer

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,19 @@
 var startQuiz = document.querySelector(".btn");
+var startTimer = document.querySelector("#starttime")
 
 function quiz() {
-    alert("test");
+    update = setInterval("timer()", 1000);
 }
 
+var time = 90;
+
+function timer() {
+    time = time - 1;
+    if (time < 90) {
+        startTimer.innerHTML = time;
+    }
+    if (time < 0) {
+        window.clearInterval(update);
+    }
+}
 startQuiz.addEventListener("click", quiz);
