af221d737a353a9b73e6e8cf27e302d6854aacb6
Aleksandr Saiyan <anverch@gmail.com>
2020-03-30 23:20:25 -0500

    add question navigation

--- Assets/script.js
+++ Assets/script.js
@@ -4,15 +4,18 @@ var startTimer = document.querySelector("#startTime");
 var currentQuestionIndex = 0;
 
 function quiz() {
-    update = setInterval("timer()", 1000);
+    update = setInterval(timer, 1000);
     document.querySelector("#startPage").classList.add("hide");
     document.querySelector("#questionsSection").classList.remove("hide");
 
     displayQuestion(questions[currentQuestionIndex], currentQuestionIndex);
 }
-
+//Timer 
 var time = 90;
 
+/**
+ * timer to count down the game time
+ */
 function timer() {
     time = time - 1;
     if (time < 90) {
@@ -22,17 +25,22 @@ function timer() {
         window.clearInterval(update);
     }
 }
+//Starts teh Quiz
 startQuiz.addEventListener("click", quiz);
 
 // Function for the question section
 function displayQuestion(question, index) {
+
     // Fields that we will change in HTML
     var questionHeader = document.querySelector("#questionHeader");
     var questionDiv = document.querySelector("#question");
     var answersDiv = document.querySelector("#answers");
 
+    // Assigns title for the question
     questionHeader.innerHTML = `Question: ${ index + 1}`;
     questionDiv.innerHTML = question.questionText;
+
+    // Loops the options for the question
     question.options.forEach(option => {
         var element = document.createElement("div");
         var button = document.createElement("button")
@@ -41,8 +49,29 @@ function displayQuestion(question, index) {
         element.appendChild(button);
         element.setAttribute("class", "row p-1");
         answersDiv.appendChild(element);
+
+        // When clicked, checks for answer
+        button.addEventListener("click", checkAnswer);
     });
 }
+
+function checkAnswer() {
+    nextQuestion();
+}
+
+function nextQuestion() {
+    if (currentQuestionIndex < 3) {
+        currentQuestionIndex += 1;
+        var answersDiv = document.querySelector("#answers");
+        answersDiv.innerHTML = ""
+        displayQuestion(questions[currentQuestionIndex], currentQuestionIndex);
+
+    } else {
+        alert("END");
+    }
+
+}
+
 
 // Array of questions/answers 
 var questions = [{
@@ -56,37 +85,28 @@ var questions = [{
     },
     {
         questionText: 'Which one below is NOT a JavaScrip Variable',
-        options: [{
-                text: 'const'
-            },
+        options: [
+            { text: 'const' },
             { text: 'start' },
-            {
-                text: 'let'
-            }
+            { text: 'let' }
         ],
         answer: 'start'
     },
     {
         questionText: 'In HTML, JavaScript code must be inserted between which tags.',
-        options: [{
-                text: '<script></script>'
-            },
-            { text: '<header></header>' },
-            {
-                text: '<footer></footer>'
-            }
+        options: [
+            { text: "script" },
+            { text: "header" },
+            { text: "footer" }
         ],
-        answer: '<script></script>'
+        answer: "script"
     },
     {
         questionText: 'In CSS ___ are reusable styles that can be added to HTML elements.',
-        options: [{
-                text: 'classes'
-            },
+        options: [
+            { text: 'classes' },
             { text: 'IDs' },
-            {
-                text: 'elements'
-            }
+            { text: 'elements' }
         ],
         answer: 'classes'
     }
