70a748d39447d95aac85bb6c44b00c6f52e6da07
Aleksandr Saiyan <anverch@gmail.com>
2020-03-30 22:45:36 -0500

    add questton btn

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,14 @@
-var startQuiz = document.querySelector("#startquiz");
-var startTimer = document.querySelector("#starttime");
+var startQuiz = document.querySelector("#startQuiz");
+var startTimer = document.querySelector("#startTime");
+
+var currentQuestionIndex = 0;
 
 function quiz() {
     update = setInterval("timer()", 1000);
     document.querySelector("#startPage").classList.add("hide");
     document.querySelector("#questionsSection").classList.remove("hide");
+
+    displayQuestion(questions[currentQuestionIndex], currentQuestionIndex);
 }
 
 var time = 90;
@@ -20,40 +24,70 @@ function timer() {
 }
 startQuiz.addEventListener("click", quiz);
 
+// Function for the question section
+function displayQuestion(question, index) {
+    // Fields that we will change in HTML
+    var questionHeader = document.querySelector("#questionHeader");
+    var questionDiv = document.querySelector("#question");
+    var answersDiv = document.querySelector("#answers");
+
+    questionHeader.innerHTML = `Question: ${ index + 1}`;
+    questionDiv.innerHTML = question.questionText;
+    question.options.forEach(option => {
+        var element = document.createElement("div");
+        var button = document.createElement("button")
+        button.innerHTML = option.text;
+        button.setAttribute("class", "btn-secondary btn btn-block");
+        element.appendChild(button);
+        element.setAttribute("class", "row p-1");
+        answersDiv.appendChild(element);
+    });
+}
+
 // Array of questions/answers 
 var questions = [{
-        question: 'In JS, opens up a yes/no dialog and returns true/false depending on user click.',
-        options: {
-            a: 'alert()',
-            b: 'prompt()',
-            c: 'confirm()'
-        },
+        questionText: 'In JS, what opens up a yes/no dialog and returns true/false depending on user click.',
+        options: [
+            { text: 'alert()' },
+            { text: 'prompt()' },
+            { text: 'confirm()' }
+        ],
         answer: 'confirm()'
     },
     {
-        question: 'Which one below is NOT a JavaScrip Variable',
-        options: {
-            a: 'const',
-            b: 'start',
-            c: 'let'
-        },
+        questionText: 'Which one below is NOT a JavaScrip Variable',
+        options: [{
+                text: 'const'
+            },
+            { text: 'start' },
+            {
+                text: 'let'
+            }
+        ],
         answer: 'start'
     },
     {
-        question: 'In HTML, JavaScript code must be inserted between which tags.',
-        options: {
-            a: '<script></script>',
-            b: '<header></header>',
-            c: '<footer></footer>'
-        },
+        questionText: 'In HTML, JavaScript code must be inserted between which tags.',
+        options: [{
+                text: '<script></script>'
+            },
+            { text: '<header></header>' },
+            {
+                text: '<footer></footer>'
+            }
+        ],
         answer: '<script></script>'
-    } {
-        question: 'In CSS ___ are reusable styles that can be added to HTML elements.',
-        options: {
-            a: 'classes',
-            b: 'IDs',
-            c: 'elements'
-        },
+    },
+    {
+        questionText: 'In CSS ___ are reusable styles that can be added to HTML elements.',
+        options: [{
+                text: 'classes'
+            },
+            { text: 'IDs' },
+            {
+                text: 'elements'
+            }
+        ],
         answer: 'classes'
     }
 ]
